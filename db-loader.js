ad5f0ccd84dfd0320914283adbf88843f91899bc
Kyle Kurzhal <kkurzhal@ipwatch.com>
2015-04-09 15:29:59 -0500

    Began adding day tracking and request generation.

--- db-loader.js
+++ db-loader.js
@@ -1,4 +1,4 @@
-var DEBUG = false;
+var DEBUG = true;
 var fs = require('fs');
 //var mongo = require('mongodb').MongoClient;
 
@@ -7,6 +7,16 @@ var mysql = require('mysql');
 var fData = [];
 var sql = 'INSERT INTO <table-name> (<columns>) VALUES (<values>)';
 var fileData = fs.readFileSync('Room_Schedule.csv','utf8').split('\n');
+
+//set up the beginning dates
+var start_date = new Date('4/1/15');
+var end_date = new Date('4/30/15');
+var next_day = Date(start_date.getDate());
+
+//set up the weekday characters; the index matches the weekday int returned by Date.getDay()
+var weekdays = ['S', 'M', 'T', 'W', 'R', 'F', 'S'];
+
+var requests_arr = [];
 
 //get rid of the first array element since it contains column headings.
 fileData.shift();
@@ -17,7 +27,34 @@ for(line in fileData)
 {
 	//split the data by commas into an array of values
 	//we need to do a check here for duplicate data
-	fData.push(fileData[line].split(','));
+//	fData.push(fileData[line].split(','));
+
+	//get the current line, remove all double quote characters with a regular expression, and then split into an array by commas
+	new_request = {};
+	var line_array = fileData[line].replace(/\"/g, '').split(',');
+
+	//create a new request from the line with an empty day (this is for creating one request for each day)
+	var new_request = 
+	{
+		building: line_array[0],
+		room: line_array[1],
+		first_name: line_array[5],
+		last_name: line_array[6],
+		start_time: line_array[12],
+		stop_time: line_array[13],
+		day: ''
+	}
+
+	//get each day
+	for(int index = 7; index < 12; ++index)
+	{
+		//create a new request for each day that is requested
+		if(line_array[index] == weekdays[index + 1])
+		{
+			new_request.day = line_array[index];
+			requests_arr.push(new_request);
+		}
+	}
 
 	if(DEBUG)
 	{
